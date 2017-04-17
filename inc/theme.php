3e01165a1dd8e7b927ff5f4883b36150c1d237a6
Austin Crane <austincrane@gmail.com>
2017-04-17 16:11:28 -0400

    remove menus

--- inc/theme.php
+++ inc/theme.php
@@ -176,3 +176,29 @@ add_filter('wp_nav_menu_objects', 'ac_first_and_last_menu_class');
 
 show_admin_bar(false);
 
+function remove_menus(){
+  
+  remove_menu_page( 'index.php' );                  //Dashboard
+  remove_menu_page( 'jetpack' );                    //Jetpack* 
+  remove_menu_page( 'edit.php' );                   //Posts
+  remove_menu_page( 'upload.php' );                 //Media
+  remove_menu_page( 'edit.php?post_type=page' );    //Pages
+  remove_menu_page( 'edit-comments.php' );          //Comments
+  remove_menu_page( 'tools.php' );                  //Tools
+
+  
+}
+add_action( 'admin_menu', 'remove_menus' );
+
+function remove_menus_for_non_admin(){
+  
+  if( !is_admin() ) {
+    remove_menu_page( 'themes.php' );                 //Appearance
+    remove_menu_page( 'plugins.php' );                //Plugins
+    remove_menu_page( 'users.php' );                  //Users
+    remove_menu_page( 'options-general.php' );        //Settings
+  }
+  
+}
+add_action( 'admin_menu', 'remove_menus_for_non_admin' );
+
